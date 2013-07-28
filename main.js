a68e95ac718ad995d11f884e9d7b81a8fad62f19
Bart Riemens <briemens@crafity.com>
2013-07-28 17:25:44 +0200

    Added a main.js file

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+/*jslint node: true */
+"use strict";
+
+/*!
+ * crafity-http - Crafity's lightweight and easy to use http server
+ * Copyright(c) 2013 Crafity
+ * Copyright(c) 2013 Bart Riemens
+ * Copyright(c) 2013 Galina Slavova
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+module.exports = require('./lib/Webservice');
+module.exports.fullname = "crafity-http";
 module.exports.version = "0.1.0";
